dce092b62314267b965e1327c6fecdabed0a299b
Yago Henrique Fernandes de Almeida <yagof2421@gmail.com>
2025-04-02 20:53:45 -0300

    Impl: Creating Modl to apply Auth method

---
+++ Models/Interfaces/UserDetails.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProStudy_NET.Models.Entities;
+
+namespace ProStudy_NET.Models
+{
+    public interface UserDetails
+    {
+        public long Id {get; set;}
+        public string UserName {get; set;}
+        public string PasswordHash {get; set;}
+
+        ICollection<Role> GetRoles{get; set;}
+    }
+}
