b5b4f9885b32a7e070f680c456e5903a9c1de77f
Yago Henrique Fernandes de Almeida <yagof2421@gmail.com>
2025-04-02 20:54:07 -0300

    Impl: Model for response and request

---
+++ Models/DTO/LoadUserDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProStudy_NET.Models.DTO
+{
+    public class LoadUserDTO
+    {
+        public string? UserName {get; set;}
+        public string? Email ;
+    }
+}
