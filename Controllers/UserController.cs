3d6ee81e60610270052fe8c22f268b695bbd6ffa
Yago Henrique Fernandes de Almeida <yagof2421@gmail.com>
2025-04-02 20:56:25 -0300

    Impr: Change Controller base return to DTO type

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,9 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using ProStudy_NET.Models.DTO;
+using ProStudy_NET.Services;
+using ProStudy_NET.Services.Interfaces;
 
 [ApiController]
 [Route("[controller]")]
 public class UserController: ControllerBase{
 
-    [HttpGet]
-    public string Get() => "Hello";
+    private readonly iUserServices userServices;
+
+    public UserController(iUserServices userServices){
+        this.userServices = userServices;
+    }
+
+    [HttpGet("/load/{username}")]
+    public ActionResult<LoadUserDTO> GetByUserName([FromRoute] string username) 
+    {
+        try
+        {
+            LoadUserDTO userDTO = userServices.GetByUserName(username);
+            return Ok(userDTO);
+        }
+        catch(ArgumentNullException ex){
+            return NotFound(ex.Message);
+        }
+        
+    }
 }
